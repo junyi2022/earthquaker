24a60c294024386a7a1d2dacfb94790be84875be
Emily Zhou <zhou@middlebury.edu>
2024-04-17 16:33:14 -0400

    scripts for data loading and prep

---
+++ extract_eq_data.py
@@ -0,0 +1,32 @@
+import pathlib
+import requests
+from google.cloud import storage
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+DATA_DIR = pathlib.Path(__file__).parent / 'raw_data'
+
+# Download the earthquak data
+url = ('https://earthquake.usgs.gov/earthquakes/feed/v1.0/'
+       'summary/all_month.geojson')
+filename = DATA_DIR / 'past_month_earthquakes.geojson'
+
+response = requests.get(url)
+response.raise_for_status()
+
+with open(filename, 'wb') as f:
+    f.write(response.content)
+
+print(f'Downloaded {filename}')
+
+# Upload the downloaded file to cloud storage
+bucket_name = os.getenv('DATA_LAKE_BUCKET')
+blob_name = 'raw/past_month_eq/past_month_earthquakes.geojson'
+storage_client = storage.Client()
+bucket = storage_client.bucket(bucket_name)
+blob = bucket.blob(blob_name)
+blob.upload_from_filename(filename)
+
+print(f'uploaded {blob_name} to {bucket_name}')
---
+++ load_eq_data.py
@@ -0,0 +1,24 @@
+import os
+from google.cloud import bigquery
+from dotenv import load_dotenv
+load_dotenv()
+
+bucket_name = os.getenv('DATA_LAKE_BUCKET')
+dataset_name = os.getenv('DATA_LAKE_DATASET')
+
+# Load the data into BigQuery as an external table
+prepared_blobname = 'prepared/past_month_eq/past_month_earthquakes.jsonl'
+table_name = 'past_month_earthquakes'
+table_uri = f'gs://{bucket_name}/{prepared_blobname}'
+
+create_table_query = f'''
+CREATE OR REPLACE EXTERNAL TABLE {dataset_name}.{table_name}
+OPTIONS (
+  format = 'JSON',
+  uris = ['{table_uri}']
+)
+'''
+
+bigquery_client = bigquery.Client()
+bigquery_client.query_and_wait(create_table_query)
+print(f'Loaded {table_uri} into {dataset_name}.{table_name}')
