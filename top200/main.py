7c272e33f97b5eaa84dae22888b1af8ab01c707e
Emily Zhou <zhou@middlebury.edu>
2024-04-27 15:11:27 -0400

    change to top 200

--- top200/main.py
+++ top200/main.py
@@ -9,13 +9,13 @@ load_dotenv()
 def load_top_earthquakes(request):
     client = bigquery.Client()
     eq_query = """
-        CREATE OR REPLACE TABLE `earthquakers.derived.top_50` AS
+        CREATE OR REPLACE TABLE `earthquakers.derived.top_200` AS
         SELECT *
         FROM `earthquakers.derived.earthquake_by_continent`
         ORDER BY mag DESC
-        LIMIT 50
+        LIMIT 200
     """
     earthquake = client.query_and_wait(eq_query).to_dataframe()
-    pandas_gbq.to_gbq(earthquake, 'derived.earthquake_top_50',
+    pandas_gbq.to_gbq(earthquake, 'derived.top_200',
                       project_id="earthquakers", if_exists='replace')
     return 'Success'
